85cb36499bab2037697c2a5cbeaadd25df3fe066
Mikhail Saraykin <saraykin1996@gmail.com>
2019-04-14 23:07:40 +0700

    полифилы и другик улучшения

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import 'babel-polyfill';
 import printMe from './print.js';
 
 function component() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,64 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
 	entry: {
+		polyfills: './src/polyfills.js',
 		app: './src/index.js',
 		print: './src/print.js',
+		another: './src/another-module.js',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
 			title: 'Output Management',
 		}),
+		new webpack.ProvidePlugin({
+			_: 'lodash',
+		}),
 	],
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				commons: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+			},
+		},
+	},
+	externals: {
+		lodash: {
+			commonjs: 'lodash',
+			commonjs2: 'lodash',
+			amd: 'lodash',
+			root: '_',
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				include: path.resolve(__dirname, 'src'),
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+		],
 	},
 };
